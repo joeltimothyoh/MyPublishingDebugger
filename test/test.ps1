ade10323cb329df2d0c32e53bfa6d8e462a46931
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-12 03:45:09 +0800

    Fix issue with pester version check leading to repeated installations

--- test/test.ps1
+++ test/test.ps1
@@ -7,8 +7,9 @@ $ErrorActionPreference = 'Stop'
 
 "Installing dependencies" | Write-Host
 # Install Pester if needed
+$pesterMinimumVersion = [version]'4.0.0'
 $pester = Get-Module Pester -ListAvailable -ErrorAction SilentlyContinue
-if ( ! $pester -or $pester.Version.Major -lt 4 ) {
+if (!$pester -or !($pester.Version -gt $pesterMinimumVersion)) {
     Install-Module Pester -Force -Scope CurrentUser
 }
 Get-Module Pester -ListAvailable
