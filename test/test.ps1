8bfbc9f00751781e2f85277179fc0c109c0c86b3
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-12 03:47:27 +0800

    Tweak pester check output and syntax

--- test/test.ps1
+++ test/test.ps1
@@ -5,12 +5,15 @@ Set-StrictMode -Version Latest
 $global:PesterDebugPreference_ShowFullErrors = $true
 $ErrorActionPreference = 'Stop'
 
-"Installing dependencies" | Write-Host
+"Installing test dependencies" | Write-Host
+
 # Install Pester if needed
+"Checking Pester version" | Write-Host
 $pesterMinimumVersion = [version]'4.0.0'
-$pester = Get-Module Pester -ListAvailable -ErrorAction SilentlyContinue
+$pester = Get-Module 'Pester' -ListAvailable -ErrorAction SilentlyContinue
 if (!$pester -or !($pester.Version -gt $pesterMinimumVersion)) {
-    Install-Module Pester -Force -Scope CurrentUser
+    "Installing Pester" | Write-Host
+    Install-Module -Name 'Pester' -Scope CurrentUser -Force
 }
 Get-Module Pester -ListAvailable
 
