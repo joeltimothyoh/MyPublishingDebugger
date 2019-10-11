0bbd188ee31acc4a37db2e227405a8bafa53b952
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-12 04:36:10 +0800

    Be more explicit in test entrypoint script with Pester module to be installed

--- test/test.ps1
+++ test/test.ps1
@@ -13,7 +13,7 @@ $pesterMinimumVersion = [version]'4.0.0'
 $pester = Get-Module 'Pester' -ListAvailable -ErrorAction SilentlyContinue
 if (!$pester -or !($pester.Version -gt $pesterMinimumVersion)) {
     "Installing Pester" | Write-Host
-    Install-Module -Name 'Pester' -Scope CurrentUser -Force
+    Install-Module -Name 'Pester' -Repository 'PSGallery' -MinimumVersion $pesterMinimumVersion  -Scope CurrentUser -Force
 }
 Get-Module Pester -ListAvailable
 
